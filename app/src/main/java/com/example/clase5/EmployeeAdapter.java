9826b99ab16e751f534e941aea1a63ea61721762
stuardo asus-tuf <stuardo.lucho@pucp.edu.pe>
2023-09-11 22:57:51 +0100

    cargar lista con botón

---
+++ app/src/main/java/com/example/clase5/EmployeeAdapter.java
@@ -0,0 +1,86 @@
+package com.example.clase5;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.clase5.entity.Employee;
+
+import java.util.List;
+
+public class EmployeeAdapter extends RecyclerView.Adapter<EmployeeAdapter.EmployeeViewHolder> {
+
+    private List<Employee> listaEmpleados;
+    private Context context;
+
+    @NonNull
+    @Override
+    public EmployeeViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View inflate = LayoutInflater.from(context).inflate(R.layout.irv_employee, parent, false);
+        return new EmployeeViewHolder(inflate);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull EmployeeViewHolder holder, int position) {
+        Employee employee = listaEmpleados.get(position);
+        holder.employee = employee;
+
+        TextView tvFirstName = holder.itemView.findViewById(R.id.textViewFirstName);
+        TextView tvLastName = holder.itemView.findViewById(R.id.textViewLastName);
+        TextView tvSalary = holder.itemView.findViewById(R.id.textViewSalary);
+
+        tvFirstName.setText(employee.getFirstName());
+        tvLastName.setText(employee.getLastName());
+        tvSalary.setText(String.valueOf(employee.getSalary()));
+
+        if (employee.getSalary() >= 10000) {
+            tvSalary.setTextColor(Color.RED);
+        } else {
+            tvSalary.setTextColor(Color.BLACK);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return listaEmpleados.size();
+    }
+
+    public class EmployeeViewHolder extends RecyclerView.ViewHolder {
+
+        Employee employee;
+
+        public EmployeeViewHolder(@NonNull View itemView) {
+            super(itemView);
+            Button button = itemView.findViewById(R.id.button2);
+            button.setOnClickListener(view -> {
+                Intent intent = new Intent(context,EmployeeDetailActivity.class);
+                context.startActivity(intent);
+            });
+        }
+    }
+
+
+    public List<Employee> getListaEmpleados() {
+        return listaEmpleados;
+    }
+
+    public void setListaEmpleados(List<Employee> listaEmpleados) {
+        this.listaEmpleados = listaEmpleados;
+    }
+
+    public Context getContext() {
+        return context;
+    }
+
+    public void setContext(Context context) {
+        this.context = context;
+    }
+}
